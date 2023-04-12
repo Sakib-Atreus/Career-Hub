28ada7669d3cb76382e2a9774b87a37170b3e24d
Sakib-Atreus <sakibatreus@gmail.com>
2023-04-12 23:05:08 +0600

    error file updated

--- src/components/Error/NoData.jsx
+++ src/components/Error/NoData.jsx
@@ -4,7 +4,7 @@ const NoData = () => {
   return (
     <div className="min-h-[calc(100vh-200px)]  flex justify-center items-center">
       <h1 className="animate-text bg-gradient-to-r from-pink-500 via-purple-500 lg:text-5xl text-3xl to-green-400 bg-clip-text text-transparent font-black ">
-        You Can't Applied Any Job!
+        You Haven't Applied To Any Job Yet!
       </h1>
     </div>
   );
